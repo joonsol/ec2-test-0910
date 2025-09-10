ac331701c664e5b51a49fdde0cdf83b159d08c5e
joonsol <71648638+joonsol@users.noreply.github.com>
2025-09-10 13:56:22 +0900

    .

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express =require('express')
+const app=express()
+const PORT = process.env.PORT
+app.use(express.json());
+app.get('/',(req,res)=>{
+  res.send(`.env 테스트:${process.env.DATABASE_NAME}`)
+})
+
+app.listen(PORT,()=>{
+  console.log(`Example app listening on port ${PORT}`)
+})
